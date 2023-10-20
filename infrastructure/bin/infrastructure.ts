14fbbe0f568819389de2748c607c1050b4dffb77
Dries Hooghe <dries@drieshooghe.com>
2023-10-20 17:43:48 +0200

    :sparkles: add CDK infrastructure to the app

---
+++ infrastructure/bin/infrastructure.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { WebsiteStack as WebsiteStack } from '../lib/website-stack';
+
+const { CDK_DEFAULT_ACCOUNT: account, CDK_DEFAULT_REGION: region } = process.env;
+
+const app = new cdk.App();
+
+new WebsiteStack(app, 'OcodaWebsiteStack', { env: { account, region } });
