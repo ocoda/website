6ff7fb6fcd6302049d582e111615cce43031af2f
Dries Hooghe <dries@drieshooghe.com>
2023-10-21 16:37:29 +0200

    :bricks: create basic infrastructure setup

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -3,8 +3,6 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { WebsiteStack } from '../lib/website-stack';
 
-const { CDK_DEFAULT_ACCOUNT: account, CDK_DEFAULT_REGION: region } = process.env;
-
 const app = new cdk.App();
 
-new WebsiteStack(app, 'OcodaWebsiteStack', { env: { account, region } });
+new WebsiteStack(app, 'OcodaWebsiteStack', { env: { region: 'us-east-1' } });
--- infrastructure/lib/website-stack.ts
+++ infrastructure/lib/website-stack.ts
@@ -1,63 +1,114 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Duration } from 'aws-cdk-lib';
 import { Certificate, CertificateValidation, ICertificate } from 'aws-cdk-lib/aws-certificatemanager';
 import {
+	AllowedMethods,
+	CachePolicy,
 	Distribution,
 	HttpVersion,
+	IDistribution,
+	LambdaEdgeEventType,
 	OriginRequestPolicy,
 	SecurityPolicyProtocol,
 	ViewerProtocolPolicy,
 } from 'aws-cdk-lib/aws-cloudfront';
 import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { HostedZone } from 'aws-cdk-lib/aws-route53';
-import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
+import { BlockPublicAccess, Bucket, IBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
+import { join } from 'path';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { BucketDeployment, CacheControl, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { getDomainName } from './utils';
+import { Stack, StackProps } from './constructs';
 
 export class WebsiteStack extends Stack {
-	readonly stage: 'staging' | 'production';
+	readonly domainName: string;
+	readonly buildPath: string;
 
 	constructor(scope: Construct, id: string, props?: StackProps) {
 		super(scope, id, props);
 
-		this.stage = this.setStage();
+		this.buildPath = join(__dirname, '../../apps/web/build');
+		this.domainName = getDomainName(this.stage);
 
-		const domainName = `ocoda.${this.stage === 'production' ? 'be' : 'dev'}`;
+		const certificate = this.createCertificate();
+		const bucket = this.createAssetBucket();
 
-		const certificate = this.createCertificate(domainName);
-		// const bucket = this.createAssetBucket();
-		// this.createDistribution(bucket);
+		const serverFunction = this.createRemixServerFunction();
+		const distribution = this.createDistribution(bucket, certificate, serverFunction);
+
+		this.createRemixBucketDeployment(bucket, distribution);
 	}
 
-	private setStage() {
-		const stage = this.node.tryGetContext('stage');
-
-		if (!['staging', 'production'].includes(stage)) {
-			throw new Error('You must specify a stage (staging or production)');
-		}
-
-		return stage;
-	}
-
-	private createCertificate(domainName: string): ICertificate {
-		const hostedZone = HostedZone.fromLookup(this, 'OcodaHostedZone', { domainName });
+	private createCertificate(): ICertificate {
+		const hostedZone = HostedZone.fromLookup(this, 'OcodaHostedZone', { domainName: this.domainName });
 		return new Certificate(this, 'OcodaWebsiteCertificate', {
-			domainName,
+			domainName: this.domainName,
 			validation: CertificateValidation.fromDns(hostedZone),
 		});
 	}
 
 	private createAssetBucket() {
-		return new Bucket(this, 'AssetsBucket', { blockPublicAccess: BlockPublicAccess.BLOCK_ALL });
+		return new Bucket(this, 'OcodaWebsiteAssetsBucket', { blockPublicAccess: BlockPublicAccess.BLOCK_ALL });
 	}
 
-	private createDistribution(bucket: Bucket) {
-		return new Distribution(this, 'AssetsBucketDistribution', {
+	private createDistribution(bucket: IBucket, certificate: ICertificate, serverFunction: NodejsFunction) {
+		const origin = new S3Origin(bucket);
+
+		return new Distribution(this, 'OcodaWebsiteDistribution', {
+			comment: 'Ocoda website distribution',
+			domainNames: [this.domainName],
+			certificate,
 			defaultBehavior: {
-				origin: new S3Origin(bucket),
+				origin,
+				originRequestPolicy: OriginRequestPolicy.ALL_VIEWER,
+				allowedMethods: AllowedMethods.ALLOW_ALL,
+				cachePolicy: CachePolicy.CACHING_DISABLED,
+				compress: true,
 				viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-				originRequestPolicy: OriginRequestPolicy.CORS_S3_ORIGIN,
+				edgeLambdas: [
+					{
+						eventType: LambdaEdgeEventType.ORIGIN_REQUEST,
+						functionVersion: serverFunction.currentVersion,
+						includeBody: true,
+					},
+				],
 			},
 			httpVersion: HttpVersion.HTTP2_AND_3,
 			minimumProtocolVersion: SecurityPolicyProtocol.TLS_V1_2_2021,
+			additionalBehaviors: {
+				'assets/*': {
+					allowedMethods: AllowedMethods.ALLOW_GET_HEAD,
+					cachePolicy: CachePolicy.CACHING_OPTIMIZED,
+					compress: true,
+					origin,
+					viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+					originRequestPolicy: OriginRequestPolicy.CORS_S3_ORIGIN,
+				},
+			},
+		});
+	}
+
+	private createRemixServerFunction() {
+		return new LambdaFunction(this, 'OcodaWebsiteServerFunction', {
+			description: 'Ocoda website remix server function',
+			runtime: Runtime.NODEJS_18_X,
+			handler: 'index.handler',
+			code: Code.fromAsset(join(this.buildPath, 'lambda-server')),
+			memorySize: 256,
+			logRetention: RetentionDays.THREE_DAYS,
+		});
+	}
+
+	private createRemixBucketDeployment(bucket: IBucket, distribution: IDistribution) {
+		return new BucketDeployment(this, 'OcodaWebsiteAssetsDeployment', {
+			destinationBucket: bucket,
+			distribution,
+			prune: true,
+			sources: [Source.asset(join(this.buildPath, 'public'))],
+			cacheControl: [CacheControl.maxAge(Duration.days(365)), CacheControl.sMaxAge(Duration.days(365))],
 		});
 	}
 }
