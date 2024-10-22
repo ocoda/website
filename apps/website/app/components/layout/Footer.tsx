5c40c034ae2d41c0c8c187c3d38c95db4c989066
Dries Hooghe <dries@drieshooghe.com>
2024-10-22 17:32:16 +0200

    💄 refactor footer

--- apps/website/app/components/layout/Footer.tsx
+++ apps/website/app/components/layout/Footer.tsx
@@ -1,20 +1,24 @@
 import type { FC } from 'react';
+import { Link } from '@remix-run/react';
 
 export const Footer: FC = () => {
   return (
     <footer className="bg-white border-t text-gray-400 text-sm">
-      <div className="container mx-auto px-8 py-4 grid md:grid-cols-3 items-center">
+      <div className="container mx-auto px-8 py-4 grid grid-cols-2 items-center">
         <div className="flex flex-col gap-2">
           <span>Ocoda BV</span>
           <span>hello@ocoda.be</span>
           <span>VAT: BE 0123.456.7890</span>
         </div>
-        <div>Links</div>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 col-start-3 items-end">
           <span>Terms & Conditions</span>
           <span>Privacy Policy</span>
-          <span>Illustrations by GetIllustrations</span>
-          <span>Image by </span>
+          <span>
+            Illustrations from{' '}
+            <Link to={'https://www.reshot.com'} target="_blank">
+              Reshot
+            </Link>
+          </span>
         </div>
       </div>
     </footer>
