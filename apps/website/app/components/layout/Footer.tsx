e501a0f8e799737bdff88a4646ba133a235f51f2
Dries Hooghe <dries@drieshooghe.com>
2024-10-28 16:43:24 +0100

    💄 restyle footer

--- apps/website/app/components/layout/Footer.tsx
+++ apps/website/app/components/layout/Footer.tsx
@@ -7,14 +7,18 @@ export const Footer: FC = () => {
   const { t, i18n } = useTranslation('common', { keyPrefix: 'footer' });
 
   return (
-    <footer className="bg-white border-t text-gray-400 text-sm">
-      <div className="items-center grid grid-cols-2 mx-auto px-8 py-4 container">
+    <footer className="bg-white border-t text-gray-500 text-xs md:text-sm">
+      <div className="items-center gap-1 md:gap-2 grid grid-cols-2 mx-auto px-8 py-8 container">
         <div className="flex flex-col gap-2">
           <span>Ocoda BV</span>
-          <Link to="mailto:hello@ocoda.be">hello@ocoda.be</Link>
-          <span>VAT: BE 0123.456.7890</span>
+          <span>
+            <Link to="mailto:hello@ocoda.be">hello@ocoda.be</Link>
+          </span>
+          <span>
+            VAT: <span className="text-nowrap">BE 0123.456.7890</span>
+          </span>
         </div>
-        <div className="flex flex-col items-end gap-2 col-start-3">
+        <div className="flex flex-col items-end gap-2">
           <Link to={ensureLocalizedURL('/terms-and-conditions', i18n.language)}>{t('terms')}</Link>
           <Link to={ensureLocalizedURL('/privacy-policy', i18n.language)}>{t('privacy')}</Link>
           <span>
