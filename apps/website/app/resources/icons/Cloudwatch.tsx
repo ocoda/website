845d1f974b0b4030cfb2f10f0fdebd8b0e4c0ccb
Dries Hooghe <dries@drieshooghe.com>
2024-11-06 21:31:13 +0100

    🍱 add icons

---
+++ apps/website/app/resources/icons/Cloudwatch.tsx
@@ -0,0 +1,19 @@
+import type { FC, SVGProps } from 'react';
+
+export const CloudwatchIcon: FC<SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 128 128"
+    fillRule="evenodd"
+    clipRule="evenodd"
+    fill="currentColor"
+    {...props}
+  >
+    <title>Cloudwatch</title>
+    <rect x="2.694" y="113.485" width="122.112" height="14.015" />
+    <rect x="7.308" y="37.669" width="26.312" height="65.447" />
+    <rect x="48.852" y="-0" width="27.284" height="103.116" />
+    <rect x="88.447" y="56.128" width="27.284" height="46.99" />
+  </svg>
+);
